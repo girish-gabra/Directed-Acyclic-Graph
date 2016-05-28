c3da6787cd63ea6f0e17f5cb0db2b88ee1f903da
girish-gabra <girish.gabra10@gmail.com>
2016-05-28 11:38:16 -0400

    Added logic to check cycle.
    Added logic to check if graph contains cycle after adding the new edge.

--- DirectedAcyclicGraph.java
+++ DirectedAcyclicGraph.java
@@ -41,7 +41,73 @@ public class DirectedAcyclicGraph {
 		neighbours.add(destination);
 		neighbourMap.put(source, neighbours);
 		
-
+		/*check if added edge causes graph to be cyclic. If so, return false and remove
+		  the added edge */
+		if(hasCycle()){
+			List<String> temp = neighbourMap.get(source);
+			temp.remove(destination);
+			return false;
+			
+		}
+		
 		return true;
-	}	
+	}
+	
+	private boolean hasCycle()
+	{
+		Set<String> whiteset = new HashSet<String>();
+		Set<String> blackset = new HashSet<String>();
+		Set<String> greyset = new HashSet<String>();
+		
+		for( String node : nodes){
+			whiteset.add(node);
+		}
+		
+		for(int i=0; i<whiteset.size();i++)
+		{
+			String currentNode = whiteset.iterator().next();
+			if(dfs(currentNode,whiteset,blackset,greyset)){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	private boolean dfs(String currentNode, Set<String> whiteset, Set<String> blackset, Set<String> greyset)
+	{
+		shiftVertex(currentNode,whiteset, greyset);
+		
+		
+		if(neighbourMap.containsKey(currentNode)){
+			List<String> neighbours = neighbourMap.get(currentNode);
+			for(String node:neighbours)
+			{
+				if (blackset.contains(node)) {
+	                continue;
+	            }
+	            //if in gray set then cycle found.
+	            if (greyset.contains(node)) {
+	                return true;
+	            }
+	            if(dfs(node, whiteset, blackset, greyset)) {
+	                return true;
+	            }
+				
+				
+			}
+	
+		}
+		
+		shiftVertex(currentNode,greyset,blackset);
+		return false;
+		
+	}
+	
+	private void shiftVertex(String currentNode, Set<String> oldset, Set<String> newset)
+	{
+		oldset.remove(currentNode);
+		newset.add(currentNode);
+	}
+	
 }
